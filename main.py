8732337412e931cc5eecaa7c33f06d7bce069423
Hüseyin Daş <hsyndass@gmail.com>
2021-12-20 18:14:47 +0300

    L6: cogs folders name changed to src.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
-from discord.ext import commands
+import discord
+import random
+from discord.ext import commands, tasks
 
 from config.settings import TOKEN, PREFIX
 from utils.cogs import LoadCogs
@@ -9,14 +11,22 @@ client = commands.Bot(command_prefix=(PREFIX))
 @client.event
 async def on_ready():
     logging.info("Bot is ready...")
+    status.start()
+
 
 @client.command()
 async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
+    client.unload_extension(f'src.{extension}')
+    client.load_extension(f'src.{extension}')
 
 
-LoadCogs(client, './cogs')
+@tasks.loop(seconds=10.0)
+async def status():
+    statuses = ["with you!", "with husoopy!", "with humans!"]
+    await client.change_presence(activity=discord.Game(random.choice(statuses)))
+
+
+LoadCogs(client, './src')
+
 logging.info("Bot is initializing...")
-
 client.run(TOKEN)
